9cc1d7ff2ce0f42c9f494dbc4f5a32044f13248a
Eliud <utti232008@utvco.edu.mx>
2026-05-21 07:53:36 -0600

    feat(home) se agrego el home

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './home.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Home {}
